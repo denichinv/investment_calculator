62e3129961124c1058b471f4c72fd00af0776420
Vilizar Denichin <denichinv@gmail.com>
2025-01-15 21:52:16 +0000

    lift state and logic from UserInput to App for centralized management

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,72 @@
 import Header from './components/Header'
 import UserInput from './components/UserInput';
+import React, { useState } from 'react';
+
 
 import image from './assets/SEO-Cost-Calculator-1.png';
 
 
 function App() {
 
+  const [userInput, setUserInput] = useState({
+         initialInvestment: 10000,
+         annualInvestment: 1200,
+         expectedReturn: 6,
+         duration: 10,
+     });
+ 
+     const [error, setError] = useState({
+         initialInvestment: "",
+         annualInvestment: "",
+         expectedReturn: "",
+         duration: "",
+     });
+ 
+     const [currency, setCurrency] = useState("USD");
+ 
+     const currencyRates = {
+         USD: 1,      
+         EUR: 0.91,    
+         GBP: 0.78,    
+     };
+     
+    const isValid = Object.values(error).every((err) => err === "") && 
+    Object.values(userInput).every((value) => value > 0);
 
+ 
+     function handleChange(id, newValue) {
+      setUserInput((prev) => ({
+          ...prev,
+          [id]: newValue,
+      }));
+
+
+      if (!newValue || newValue <= 0) {
+          setError((prevError) => ({
+              ...prevError,
+              [id]: "Value must be greater than 0",
+          }));
+      } else {
+          setError((prevError) => ({
+              ...prevError,
+              [id]: "",
+          }));
+      }
+  }
+  function handleCurrencyChange(newCurrency) {
+      const conversionRate = currencyRates[newCurrency] / currencyRates[currency]
+      setCurrency(newCurrency)
+      setUserInput((prevInput) => ({
+          initialInvestment: prevInput.initialInvestment * conversionRate,
+          annualInvestment: prevInput.annualInvestment * conversionRate,
+          expectedReturn: prevInput.expectedReturn,
+          duration: prevInput.duration,
+      }));
+    }
   return (
     <>
     <Header logo={image}/>
-    <UserInput/>
+    <UserInput userInput={userInput} error={error} currency={currency} handleChange={handleChange} handleCurrencyChange={handleCurrencyChange} isValid={isValid}/>
 
 
     </>
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,63 +1,12 @@
-import React, { useState } from 'react';
+/* eslint-disable react/prop-types */
+import React from 'react';
 import "./userInput.css";
 
-const UserInput = () => {
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 6,
-        duration: 10,
-    });
-
-    const [error, setError] = useState({
-        initialInvestment: "",
-        annualInvestment: "",
-        expectedReturn: "",
-        duration: "",
-    });
-
-    const [currency, setCurrency] = useState("USD");
-
-    const currencyRates = {
-        USD: 1,      
-        EUR: 0.91,    
-        GBP: 0.78,    
-    };
+const UserInput = ({userInput,error,currency,handleChange,handleCurrencyChange, isValid}) => {
 
 
-    function handleChange(id, newValue) {
-        setUserInput((prev) => ({
-            ...prev,
-            [id]: newValue,
-        }));
 
 
-        if (!newValue || newValue <= 0) {
-            setError((prevError) => ({
-                ...prevError,
-                [id]: "Value must be greater than 0",
-            }));
-        } else {
-            setError((prevError) => ({
-                ...prevError,
-                [id]: "",
-            }));
-        }
-    }
-    function handleCurrencyChange(newCurrency) {
-        const conversionRate = currencyRates[newCurrency] / currencyRates[currency]
-        setCurrency(newCurrency)
-        setUserInput((prevInput) => ({
-            initialInvestment: prevInput.initialInvestment * conversionRate,
-            annualInvestment: prevInput.annualInvestment * conversionRate,
-            expectedReturn: prevInput.expectedReturn,
-            duration: prevInput.duration,
-        }));
-
-    }
-
-    const isValid = Object.values(error).every((err) => err === "") && 
-                    Object.values(userInput).every((value) => value > 0);
 
     console.log(userInput);
 
