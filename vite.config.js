993527c23e3f442c6484240ad83db17976be93f9
Vilizar Denichin <denichinv@gmail.com>
2025-04-06 16:46:25 +0100

    Prepare for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/password-generator/',
+  base: '/investment_calculator/',
   plugins: [react()],
 });
